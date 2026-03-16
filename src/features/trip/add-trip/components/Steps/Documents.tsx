a9c5421761c1b664a78ab5a6a67f280636adc1ea
Igor Baranov <baranow.ig@gmail.com>
2026-03-16 23:44:56 +0100

    feat: add documents editing tab with auto upload

--- src/features/trip/add-trip/components/Steps/Documents.tsx
+++ src/features/trip/add-trip/components/Steps/Documents.tsx
@@ -1,23 +1,25 @@
-import { useAppDispatch, useAppSelector } from "@store/index";
+import { useAppDispatch, useAppSelector } from '@store/index';
 
-import FilesForm from "../../../components/Files/FilesForm";
-import type { TripFile } from "../../../types";
+import FilesForm from '../../../components/Files/FilesForm';
+import type { TripFile } from '../../../types';
 import {
   nextStep,
   selectWizardTrip,
   setDocuments,
-} from "../../store/tripWizardSlice";
-import Pagination from "../Navigation/Pagination";
+} from '../../store/tripWizardSlice';
+import Pagination from '../Navigation/Pagination';
 
 export default function Documents() {
-  const { documents, onSubmit, onChange } = useDocumentsForm();
+  const { documents, onSubmit, onFileStorageRemoval, tripId } =
+    useDocumentsForm();
 
   return (
     <FilesForm
+      tripId={tripId}
       defaultFiles={documents}
       onSubmit={onSubmit}
       SubmitComponent={<Pagination />}
-      onChange={onChange}
+      onFileStorageRemoval={onFileStorageRemoval}
       type="document"
     />
   );
@@ -32,13 +34,14 @@ function useDocumentsForm() {
     dispatch(nextStep());
   };
 
-  const onChange = (data: TripFile[]) => {
+  const onFileStorageRemoval = (data: TripFile[]) => {
     dispatch(setDocuments(data));
   };
 
   return {
     onSubmit,
     documents: trip.documents,
-    onChange,
+    onFileStorageRemoval,
+    tripId: trip.id,
   };
 }
